18494d8cc295f79127a96bdc12b60079ae4f086b
arxsyy <marsyfernanda@gmail.com>
2025-09-22 22:50:55 +0700

    P4_Bagian2_No.14

--- Pertemuan4/tipe_data.php
+++ Pertemuan4/tipe_data.php
@@ -42,4 +42,7 @@ echo "Nama Depan : {$namaDepan} <br>";
 echo "Nama Belakang : " . $namaBelakang . '<br>';
 
 echo $namaLengkap;
+
+$listMahasiswa = ["Wahid Abdullah", "Elmo Bachtiar", "Lendis Fabri"];
+echo $listMahasiswa[0] ;
 ?>
