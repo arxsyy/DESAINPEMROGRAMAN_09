256a5941937224c32b2d6bf6e42f57d025347f41
arxsyy <marsyfernanda@gmail.com>
2025-09-30 16:45:18 +0700

    P5_FungsiRekursif_No.4

--- Pertemuan5/rekursif.php
+++ Pertemuan5/rekursif.php
@@ -7,8 +7,16 @@
 
 // tampilkanHaloDunia();
 
-for ($i=1; $i <=25; $i++) { 
-    echo "Perulangan ke-$i <br>";
-}
+// for ($i=1; $i <=25; $i++) { 
+//     echo "Perulangan ke-$i <br>";
+// }
 
+function tampilkanAngka (int $jumlah, int $indeks = 1) {
+    echo "Perulangan ke-{$indeks} <br>";
+
+    if ($indeks < $jumlah) {
+        tampilkanAngka($jumlah, $indeks + 1);
+    }
+}
+tampilkanAngka(20);
 ?>
