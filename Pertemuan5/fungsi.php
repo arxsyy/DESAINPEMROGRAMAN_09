18a4f46c727ad94f2a470666dad5109654596790
arxsyy <marsyfernanda@gmail.com>
2025-09-30 16:07:40 +0700

    P5_Fungsi1_No.2

---
+++ Pertemuan5/fungsi.php
@@ -0,0 +1,12 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Arsy<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan();
+perkenalan();
+
+?>
