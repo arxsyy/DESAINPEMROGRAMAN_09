33ff843414c20f1eff54e25a189798921fe1cfbe
arxsyy <marsyfernanda@gmail.com>
2025-10-08 16:06:24 +0700

    P7_Bagian5_No.15

--- Pertemuan7/regex.php
+++ Pertemuan7/regex.php
@@ -4,9 +4,9 @@
 $pattern = '/[a-z]/'; // Cocokkan huruf kecil. 
 $text = 'This is a Sample Text.';
 if (preg_match($pattern, $text)) {
-    echo "Huruf kecil ditemukan!";
+    echo "Huruf kecil ditemukan!<br>";
 } else {
-    echo "Tidak ada huruf kecil!";
+    echo "Tidak ada huruf kecil!<br>";
 }
 
 // percobaan 5.2
@@ -14,9 +14,9 @@ $pattern = '/[0-9]+/'; // Cocokkan satu atau lebih digit.
 $text = 'There are 123 apples.';
 
 if (preg_match($pattern, $text, $matches)) {
-    echo "Cocokkan: " . $matches[0] . "";
+    echo "Cocokkan: " . $matches[0] . "<br>";
 } else {
-    echo "Tidak ada yang cocok!";
+    echo "Tidak ada yang cocok!<br>";
 }
 
 // percobaan 5.3
@@ -24,15 +24,15 @@ $pattern = '/apple/';
 $replacement = 'banana';
 $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
-echo $new_text . ""; // Output: "I like banana pie."
+echo $new_text . "<br>"; // Output: "I like banana pie."
 
 // Percobaan 5.4
 $pattern = '/go*d/'; //Cocokkan "god", "good", "goood", dll.
 $text = 'god is good.';
 
 if (preg_match($pattern, $text, $matches)) {
-    echo "Cocokkan: " . $matches[0] . "";
+    echo "Cocokkan: " . $matches[0] . "<br>";
 } else {
-    echo "Tidak ada yang cocok!";
+    echo "Tidak ada yang cocok!<br>";
 }
 ?>
