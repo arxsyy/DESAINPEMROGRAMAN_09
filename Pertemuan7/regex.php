5530f9773936aa0b1080e183954c21395f254f54
arxsyy <marsyfernanda@gmail.com>
2025-10-08 16:04:48 +0700

    P7_Bagian5_No.14

--- Pertemuan7/regex.php
+++ Pertemuan7/regex.php
@@ -25,4 +25,14 @@ $replacement = 'banana';
 $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text . ""; // Output: "I like banana pie."
+
+// Percobaan 5.4
+$pattern = '/go*d/'; //Cocokkan "god", "good", "goood", dll.
+$text = 'god is good.';
+
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0] . "";
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
