2949174fff767f0b6b3bba5f39d64310905160a4
arxsyy <marsyfernanda@gmail.com>
2025-11-04 18:44:32 +0700

    P8_Bagian1_No.6

---
+++ Pertemuan8/form_upload.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>File Upload</title>
+    </head>
+    <body>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            <input type="file" name="myfile">
+            <input type="submit" name="submit" value="Upload File">
+        </form>
+    </body>
+</html>
