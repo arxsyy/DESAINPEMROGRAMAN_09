c05be6b575f75ac5c6575382878215a4365c2712
arxsyy <marsyfernanda@gmail.com>
2025-11-16 16:45:47 +0700

    P10.2_Bagian4_No.3

--- crud/proses.php
+++ crud/proses.php
@@ -35,4 +35,23 @@ elseif($aksi == "ubah"){
     }
 }
 
+elseif($aksi == 'hapus'){
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+        
+        $query = "DELETE FROM anggota WHERE id =$id";
+        
+        if (mysqli_query($koneksi, $query)) {
+            header("Location: index.php");
+            exit();
+        } else {
+            echo "Gagal menghapus data: " . mysqli_error($koneksi);
+        }
+    } else {
+        echo "ID tidak valid.";
+    }
+}else{
+    header("Location: index.php");
+}
+
 mysqli_close($koneksi);
